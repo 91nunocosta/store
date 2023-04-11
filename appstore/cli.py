65ab025cfbdbbfebb424efb6a3702923c62adfe8
91nunocosta <91nunocosta@gmail.com>
2023-04-11 11:07:37 +0100

    feat: implement CLI

--- appstore/cli.py
+++ appstore/cli.py
@@ -1,16 +1,146 @@
-"""Provide the command line interface prototype_python_library."""
-import typer
+"""Provide the command line interface for the app store's purchases manager."""
+import os
+import textwrap
+from typing import Dict, List
 
-from appstore import fib
+import appstore.accounts
+from appstore.apps import InMemoryAppsDB
+from appstore.appstore import AccountsController, AppsDB, AppStore, UsersDB
+from appstore.users import InMemoryUsersDB
 
-app: typer.Typer = typer.Typer()
+EXIT_CMDS = {"exit"}
+
+CUR = "€"
+
+APPSTORE_ID = "AptoideStore#1"
+INITIAL_BALANCE = 10.0
+APPS = ["TrivialDrive", "DiamondLegendDeveloper"]
+DEVS = ["TrivialDriveDeveloper#2", "DiamondLegendDeveloper"]
+ITEMS: List[Dict[str, float]] = [{"Oil": 1, "Antifreeze": 1.20}, {"5x_Diamonds": 2}]
+USERS = ["User#123"]
 
 
-@app.command()
-def fibbonacy(position: int) -> None:
-    """Compute the number at a certain position in the fibbonacy sequence.
+def _create_accounts() -> AccountsController:
+    accounts = appstore.accounts.AccountsController()
+    for holder_id in DEVS + USERS + [APPSTORE_ID]:
+        accounts.deposit(INITIAL_BALANCE, holder_id)
+    return accounts
 
-    Args:
-        position (int): position of fibbonacy sequence to compute.
-    """
-    typer.echo(fib(position))
+
+def _create_apps() -> AppsDB:
+    appsdb = InMemoryAppsDB()
+    for app, dev, items in zip(APPS, DEVS, ITEMS):
+        appsdb.add_app(app_id=app, developer_id=dev, items=items)
+    return appsdb
+
+
+def _create_users() -> UsersDB:
+    usersdb = InMemoryUsersDB()
+    for user in USERS:
+        usersdb.add_user(user)
+    return usersdb
+
+
+def _create_appstore(accounts: AccountsController) -> AppStore:
+    return AppStore(
+        appstore_id=APPSTORE_ID,
+        commission=0.25,
+        accounts_controller=accounts,
+        appsdb=_create_apps(),
+        usersdb=_create_users(),
+        bonus_after_purchases={1: 0.05, 10: 0.10},
+    )
+
+
+def _sell(
+    accounts: AccountsController, store: AppStore, app: str, item: str, user: str
+) -> str:
+    try:
+        sale = store.sell(app_id=app, app_item=item, user_id=user)
+        reward_text = ""
+        if sale.reward:
+            reward_text = f"reward: {CUR}{sale.reward:.2f}"
+        return textwrap.dedent(
+            f"""
+            sale:
+                id: {sale.identifier}
+                app: {app}
+                item: {item}
+                amount: {CUR}{sale.user_debit}
+                sender: {user}
+                receivers:
+                    {sale.develper_id}: {CUR}{sale.developer_credit:.2f}
+                    {APPSTORE_ID}: {CUR}{sale.store_credit:.2f}
+                {reward_text}
+            balance:
+                {user}: {CUR}{accounts.get_balance(user):.2f}
+                {sale.develper_id}: {CUR}{accounts.get_balance(sale.develper_id):.2f}
+                {APPSTORE_ID}: {CUR}{accounts.get_balance(APPSTORE_ID):.2f}
+            """
+        )
+
+    except KeyError as err:
+        _id = err.args[0]
+        if isinstance(_id, tuple):
+            _id = _id[-1]
+        return f"Couldn't find {_id}!"
+
+    except appstore.accounts.ForbiddenDebit as err:
+        return textwrap.dedent(
+            f"""
+            Can't sell an app that costs {CUR}{-1 * err.amount} to {err.holder_id}!
+            {err.holder_id}'s balance is {CUR}{err.balance}."""
+        )
+
+
+def _help() -> str:
+    items_lines = []
+    ident = "    "
+    for app, items in zip(APPS, ITEMS):
+        items_lines.append(f"{ident}- {app}:")
+        ident = "        "
+        for item in items.keys():
+            items_lines.append(f"            - {item}")
+    items_list = os.linesep.join(items_lines)
+    users_list = os.linesep.join(f"- {user}" for user in USERS)
+
+    text = f"""
+    Supported commands:
+
+        sell APP ITEM USER
+
+        exit
+
+
+    Available apps and included items:
+    {items_list}
+
+
+    Available users:
+        {users_list}
+
+
+    Initial balance for the app store, developers and users is:
+        {CUR}{INITIAL_BALANCE}"""
+
+    return text
+
+
+def run() -> None:
+    """Run apps store manager Red-Eval-Print Loop (REPL) command line interface."""
+    accounts = _create_accounts()
+    store = _create_appstore(accounts=accounts)
+
+    while True:
+        command = input(">")
+
+        if command in EXIT_CMDS:
+            return
+
+        args = command.split()
+
+        if len(args) == 4 and args[0] == "sell":
+            print(_sell(accounts, store, *args[1:]))
+
+        else:
+            print(_help())
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,13 +1,108 @@
-"""Test the command line interface for appstore."""
-from typer.testing import CliRunner
+"""Test the command line interface for app store's purchases manager."""
+import io
+import os
+from pathlib import Path
+from typing import Iterable
 
-from appstore.cli import app
+import pytest
 
-runner = CliRunner()
+from appstore.cli import run
 
 
-def test_app() -> None:
-    """Test the command line interface for appstore"""
-    result = runner.invoke(app, ["10"])
-    assert result.exit_code == 0
-    assert result.stdout.strip() == "55"
+def _get_input_files() -> Iterable[Path]:
+    _dir = Path(__file__).parent / "cli"
+    return _dir.glob("input*.txt")
+
+
+def _test_id(input_file: Path) -> str:
+    return input_file.stem
+
+
+def _output_file(input_file: Path) -> Path:
+    name = input_file.name.replace("input", "output")
+    return input_file.with_name(name)
+
+
+@pytest.mark.parametrize("input_file", argvalues=_get_input_files(), ids=_test_id)
+def test_cli(
+    monkeypatch: pytest.MonkeyPatch,
+    capsys: pytest.CaptureFixture[str],
+    input_file: Path,
+) -> None:
+    """Tests command line interface.
+
+    Args:
+        monkeypatch: Pytest patch fixture.
+        capsys: Pytest fixture for accessing stdout.
+        input_file: Text file to use as stdin.
+    """
+    monkeypatch.setattr("sys.stdin", input_file.open())
+    run()
+    captured = capsys.readouterr()
+    assert captured.out.strip() == _output_file(input_file).read_text().strip()
+
+
+def test_wrong_ids(
+    monkeypatch: pytest.MonkeyPatch, capsys: pytest.CaptureFixture[str]
+) -> None:
+    """Tests inputing wrong ids in the CLI.
+
+    Args:
+        monkeypatch: Pytest patch fixture.
+        capsys: Pytest fixture for accessing stdout.
+    """
+    commands = [
+        "sell WrongApp Oil User#123",
+        "sell TrivialDrive WrongItem User#123",
+        "sell TrivialDrive Oil WrongUser",
+        "exit",
+    ]
+    stdin = os.linesep.join(commands)
+    monkeypatch.setattr("sys.stdin", io.StringIO(stdin))
+    run()
+    captured = capsys.readouterr()
+    assert captured.out.splitlines() == [
+        ">Couldn't find Oil!",
+        ">Couldn't find WrongItem!",
+        ">Couldn't find WrongUser!",
+        ">",
+    ]
+
+
+def test_forbidden_debit(
+    monkeypatch: pytest.MonkeyPatch, capsys: pytest.CaptureFixture[str]
+) -> None:
+    """Tests triggering forbidden debits from the CLI.
+
+    Args:
+        monkeypatch: Pytest patch fixture.
+        capsys: Pytest fixture for capturing the stdout and stderr.
+    """
+    commands = ["sell TrivialDrive Oil User#123" for _ in range(11)]
+    commands.append("exit")
+    stdin = os.linesep.join(commands)
+    monkeypatch.setattr("sys.stdin", io.StringIO(stdin))
+    run()
+    captured = capsys.readouterr()
+    # For now, we assume that if the user doesn't have an account, it's balance is 0.
+    assert "Can't sell an app" in captured.out
+
+
+def test_invalid_commands(
+    monkeypatch: pytest.MonkeyPatch, capsys: pytest.CaptureFixture[str]
+) -> None:
+    """Tests inputing wrong commands in the CLI.
+
+    Args:
+        monkeypatch: Pytest patch fixture.
+        capsys: Pytest fixture for capturing the stdout and stderr.
+    """
+    commands = [
+        "wrong command",
+        "exit",
+    ]
+    stdin = os.linesep.join(commands)
+    monkeypatch.setattr("sys.stdin", io.StringIO(stdin))
+    run()
+    captured = capsys.readouterr()
+    assert "Supported commands:" in captured.out
